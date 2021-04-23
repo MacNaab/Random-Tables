c911e26ceefec3d8b4e627308d723483595fdb54
MacNaab <armand.proy@gmail.com>
2021-04-23 14:49:18 +0200

    Simulation
    Texte intro
    Fixes

--- Simulation/bundle.js
+++ Simulation/bundle.js
@@ -111,7 +111,7 @@ function afficher_gens(combat){
 			if(f.loc.dmg==3){content += "<br>Localisation: Tête"}else if(f.loc.dmg==1){content += "<br>Localisation: Torse"}else if(f.loc.dmg==0.5){content += "<br>Localisation: Membre"}else{content += "<br>Localisation: Aléatoire"}
 			content += "<br>Position: "+f.position+"<br>Stratégie: "+f.Strat;
 			var button1 = '<button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-trigger="focus" title="'+f.id+'" data-bs-placement="left" data-bs-content="'+content+'" data-bs-html="true">'+f.id+'</button>';
-			var del = '<span Groupe="'+e+'" Nom="'+f.id+'" type="button" class="Dlt"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/> </svg></span>';
+			var del = '<span Groupe="'+e+'" Nom="'+f.id+'" class="Dlt"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/> </svg></span>';
 			var edit = '<span class="trig" Groupe="'+e+'" Nom="'+f.id+'"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"> <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/> <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/> </svg></span>';
 			aff += '<div>'+button1+edit+del+'</div>';
 		});
@@ -158,11 +158,9 @@ function afficher_gens(combat){
 			$('#ModalMo').prop('checked', true);
 		}else{$('#ModalMo').prop('checked', false);}
 		myModal.toggle();
-		$('#SaveModal').on('click', function(){
+		$('#SaveModal').on('click', function(event){
 			let loc = Number($('#ModLoc').val());
-			if(loc==NaN){loc={viser:null,dmg:null};}else{
-				if(loc=3){loc={viser:-6,dmg:3};}else if(loc=1){loc={viser:-1,dmg:1};}else{loc={viser:-3,dmg:0.5};}
-			}
+			if(loc==3){loc={viser:-6,dmg:3};}else if(loc==1){loc={viser:-1,dmg:1};}else if(loc==0.5){loc={viser:-3,dmg:0.5};}else{loc={viser:null,dmg:null}}
 			combat.parties[Gr].members[index].id = $('#modalNom').val();
 			combat.parties[Gr].members[index].hp = $('#modalPS').val();
 			combat.parties[Gr].members[index].ac = $('#modalPA').val();
@@ -180,6 +178,7 @@ function afficher_gens(combat){
 			combat.parties[Gr].members[index].FP = $('#ModalFP').is(':checked');
 			combat.parties[Gr].members[index].Monster = $('#ModalMo').is(':checked');
 			afficher_gens(combat);
+			$(this).off(event);
 		});
 	});
 }
